8a1c40c10ff6be1eab4f6d3a70589a16c26116e0
kshitij-y <kshitijyadav2003@gmail.com>
2025-03-17 02:53:17 +0530

    better auth drizzle

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { cors } from "hono/cors";
+import { auth } from './utils/auth.js';
+
+const app = new Hono();
+app.use(
+  "/api/auth/*",
+  cors({
+    origin: "http://localhost:3001", // replace with your origin
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  })
+);
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => {
+  console.log("req came");
+  return auth.handler(c.req.raw);
+});
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
---
+++ src/utils/auth.ts
@@ -0,0 +1,25 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../db/index.js";
+import {schema} from "../db/schema.js"
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  usersTable: "user", // Ensure this matches your Drizzle schema
+  baseUrl: "http://localhost:3000/api/auth/",
+  jwtSecret: process.env.JWT_SECRET!,
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+
+  trustedOrigins: ["http://localhost:3000", "http://localhost:3001", "*"],
+});
